0af3330771a3adc88030717ad25ec8042a44c995
parksil0 <parksil00@naver.com>
2022-06-17 00:43:00 +0900

    feat(css): emotion theme 및 전역 스타일 설정

---
+++ styles/globalStyles.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+	html {
+		font-family: 'Neo둥근모', 'neodgm';
+	}
+
+	body {
+		margin: 0;
+		padding: 0;
+	}
+
+	h1,
+	h2,
+	h3,
+	p {
+		margin: 0;
+	}
+`;
