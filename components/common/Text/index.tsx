797d0ba21c40de013a3a17162946d7c551f011b1
parksil0 <parksil00@naver.com>
2022-06-17 11:12:43 +0900

    feat(component): 공통 컴포넌트 props 및 스타일 재설정

--- components/common/Text/index.tsx
+++ components/common/Text/index.tsx
@@ -1,64 +1,64 @@
 import theme from '@/styles/theme';
 import React from 'react';
 import {
-	Body,
-	Caption,
-	Heading,
-	PageHeading,
-	SubHeading,
+  Body,
+  Caption,
+  Heading,
+  PageHeading,
+  SubHeading,
 } from './index.styled';
 
 interface Props
-	extends React.HTMLAttributes<
-		HTMLHeadingElement | HTMLParagraphElement | HTMLSpanElement
-	> {
-	children: React.ReactNode;
-	fontWeight?: 'normal' | 'bold';
-	color?: keyof typeof theme.colors;
-	variant?: 'pageHeading' | 'heading' | 'subHeading' | 'body' | 'caption';
+  extends React.HTMLAttributes<
+    HTMLHeadingElement | HTMLParagraphElement | HTMLSpanElement
+  > {
+  children: React.ReactNode;
+  fontWeight?: 400 | 700;
+  color?: keyof typeof theme.colors;
+  variant?: 'pageHeading' | 'heading' | 'subHeading' | 'body' | 'caption';
 }
 
 const Text = React.forwardRef(
-	({
-		children,
-		variant = 'body',
-		color = 'gray_90',
-		fontWeight,
-		...rest
-	}: Props) => {
-		switch (variant) {
-			case 'pageHeading':
-				return (
-					<PageHeading color={color} fontWeight={fontWeight} {...rest}>
-						{children}
-					</PageHeading>
-				);
-			case 'subHeading':
-				return (
-					<SubHeading color={color} fontWeight={fontWeight} {...rest}>
-						{children}
-					</SubHeading>
-				);
-			case 'heading':
-				return (
-					<Heading color={color} fontWeight={fontWeight} {...rest}>
-						{children}
-					</Heading>
-				);
-			case 'caption':
-				return (
-					<Caption color={color} fontWeight={fontWeight} {...rest}>
-						{children}
-					</Caption>
-				);
-			default:
-				return (
-					<Body color={color} fontWeight={fontWeight} {...rest}>
-						{children}
-					</Body>
-				);
-		}
-	},
+  ({
+    children,
+    variant = 'body',
+    color = 'gray_90',
+    fontWeight,
+    ...rest
+  }: Props) => {
+    switch (variant) {
+      case 'pageHeading':
+        return (
+          <PageHeading color={color} fontWeight={fontWeight} {...rest}>
+            {children}
+          </PageHeading>
+        );
+      case 'subHeading':
+        return (
+          <SubHeading color={color} fontWeight={fontWeight} {...rest}>
+            {children}
+          </SubHeading>
+        );
+      case 'heading':
+        return (
+          <Heading color={color} fontWeight={fontWeight} {...rest}>
+            {children}
+          </Heading>
+        );
+      case 'caption':
+        return (
+          <Caption color={color} fontWeight={fontWeight} {...rest}>
+            {children}
+          </Caption>
+        );
+      default:
+        return (
+          <Body color={color} fontWeight={fontWeight} {...rest}>
+            {children}
+          </Body>
+        );
+    }
+  },
 );
 
 export default Text;
