4d6a0ff9b3463d4db649ea65fc46dce2f4ad1927
parksil0 <parksil00@naver.com>
2022-06-17 11:12:43 +0900

    feat(component): 공통 컴포넌트 props 및 스타일 재설정

--- components/common/Text/index.styled.ts
+++ components/common/Text/index.styled.ts
@@ -2,38 +2,46 @@ import styled from '@emotion/styled';
 import theme from '@/styles/theme';
 
 interface Props {
-	color: keyof typeof theme.colors;
-	fontWeight?: 'normal' | 'bold';
+  color: keyof typeof theme.colors;
+  fontWeight?: 400 | 700;
 }
 
 export const PageHeading = styled.h1<Props>`
-	font-size: 32px;
-	line-height: 38px;
-	font-weight: ${({ fontWeight }) => fontWeight || 700};
+  font-size: 32px;
+  line-height: 38px;
+  font-weight: ${({ fontWeight }) => fontWeight || 700};
 
-	color: ${({ theme, color }) => theme.colors[color]};
+  color: ${({ theme, color }) => theme.colors[color]};
 `;
 
 export const SubHeading = styled.h2<Props>`
-	font-size: 20px;
-	line-height: 23px;
-	font-weight: ${({ fontWeight }) => fontWeight || 400};
+  font-size: 20px;
+  line-height: 23px;
+  font-weight: ${({ fontWeight }) => fontWeight || 400};
+
+  color: ${({ theme, color }) => theme.colors[color]};
 `;
 
 export const Heading = styled.h3<Props>`
-	font-size: 14px;
-	line-height: 16px;
-	font-weight: ${({ fontWeight }) => fontWeight || 400};
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: ${({ fontWeight }) => fontWeight || 400};
+
+  color: ${({ theme, color }) => theme.colors[color]};
 `;
 
 export const Caption = styled.span<Props>`
-	font-size: 12px;
-	line-height: 18px;
-	font-weight: ${({ fontWeight }) => fontWeight || 400};
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: ${({ fontWeight }) => fontWeight || 400};
+
+  color: ${({ theme, color }) => theme.colors[color]};
 `;
 
 export const Body = styled.p<Props>`
-	font-size: 14px;
-	line-height: 21px;
-	font-weight: ${({ fontWeight }) => fontWeight || 400};
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: ${({ fontWeight }) => fontWeight || 400};
+
+  color: ${({ theme, color }) => theme.colors[color]};
 `;
