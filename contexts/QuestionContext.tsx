864343690d67ce687326145a72495b1e23db691c
parksil0 <parksil00@naver.com>
2022-07-20 23:02:30 +0900

    feat(context): mbti 문제 진행 관련 context 생성

---
+++ contexts/QuestionContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useMemo,
+  useState,
+  type Dispatch,
+  type PropsWithChildren,
+  type SetStateAction,
+} from 'react';
+import type { SelectedAnswer } from '@/types/question';
+
+const initialSelectedAnswer: SelectedAnswer = {
+  i: 0,
+  e: 0,
+  s: 0,
+  n: 0,
+  t: 0,
+  f: 0,
+  j: 0,
+  p: 0,
+} as const;
+
+interface Props {
+  currentQuestion: number;
+  setCurrentQuestion: Dispatch<SetStateAction<number>>;
+  selectedAnswers: SelectedAnswer;
+  setSelectedAnswers: Dispatch<SetStateAction<SelectedAnswer>>;
+}
+
+export const QuestionContext = createContext<Props>({
+  currentQuestion: 1,
+  setCurrentQuestion: () => {},
+  selectedAnswers: { ...initialSelectedAnswer },
+  setSelectedAnswers: () => {},
+});
+
+const QuestionProvider = ({ children }: PropsWithChildren<unknown>) => {
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [selectedAnswers, setSelectedAnswers] = useState({
+    ...initialSelectedAnswer,
+  });
+
+  const value = useMemo(
+    () => ({
+      currentQuestion,
+      setCurrentQuestion,
+      selectedAnswers,
+      setSelectedAnswers,
+    }),
+    [currentQuestion, setCurrentQuestion, selectedAnswers],
+  );
+
+  return (
+    <QuestionContext.Provider value={value}>
+      {children}
+    </QuestionContext.Provider>
+  );
+};
+
+export default QuestionProvider;
