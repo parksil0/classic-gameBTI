0f7031b157c2622d121f90ddc01b0e49ca197fc5
parksil0 <parksil00@naver.com>
2022-07-27 16:57:11 +0900

    chore(): todo 주석 추가

--- components/Question/AnswerButton/index.styles.ts
+++ components/Question/AnswerButton/index.styles.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+// TODO: theme에 색 추가
 export const Wrapper = styled.button`
   display: flex;
   width: 273px;
@@ -9,7 +10,6 @@ export const Wrapper = styled.button`
   padding: 8px 16px;
 
   margin: 33px auto 0;
-
   background: #e5eeff;
   border-radius: 4px;
 `;
