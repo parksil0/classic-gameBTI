bce01d84a597b43c2bf2e2f27f77fc377ab15cce
parksil0 <parksil00@naver.com>
2022-07-27 23:05:56 +0900

    feat(hook): mbti 결과와 함께 result 페이지로 이동하는 훅 생성

--- components/Question/index.tsx
+++ components/Question/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import ground from '@/assets/ground.png';
 import sky from '@/assets/sky.png';
@@ -8,12 +9,29 @@ import Header from './Header';
 import QuestionNumber from './QuestionNumber';
 import Title from './Title';
 import * as Styled from './index.styles';
-import { Questions } from './index.constants';
+import { MBTI_TYPES, Questions } from './index.constants';
 import AnswerButton from './AnswerButton';
 import { MbtiType } from './index.types';
 
 const Question = () => {
-  const { currentQuestion } = useContext(QuestionContext);
+  const router = useRouter();
+  const { currentQuestion, selectedAnswers, isFinished } =
+    useContext(QuestionContext);
+
+  useEffect(() => {
+    if (isFinished) {
+      const first =
+        selectedAnswers.i > selectedAnswers.e ? MBTI_TYPES.i : MBTI_TYPES.e;
+      const second =
+        selectedAnswers.n > selectedAnswers.s ? MBTI_TYPES.n : MBTI_TYPES.s;
+      const third =
+        selectedAnswers.t > selectedAnswers.f ? MBTI_TYPES.t : MBTI_TYPES.f;
+      const fourth =
+        selectedAnswers.p > selectedAnswers.j ? MBTI_TYPES.p : MBTI_TYPES.j;
+
+      router.push(`/result?mbti=${first}${second}${third}${fourth}`);
+    }
+  }, [isFinished, router, selectedAnswers]);
 
   return (
     <Styled.Wrapper
